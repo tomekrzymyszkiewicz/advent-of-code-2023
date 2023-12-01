d15c7e8710363b8a441ec8423b71c1a336885200
Tomasz Rzymyszkiewicz <tomasz.rzymyszkiewicz@nokia.com>
2023-12-01 15:04:50 +0100

    syntax

--- day01/task1.py
+++ day01/task1.py
@@ -1,9 +1,9 @@
-with open('input.txt','r') as f:
+with open("input.txt", "r") as f:
     lines = f.readlines()
-    
+
 sum = 0
 for line in lines:
     digits = [i for i in line if i.isdigit()]
-    sum += int(''.join([digits[0],digits[-1]]))
-    
+    sum += int("".join([digits[0], digits[-1]]))
+
 print(sum)
