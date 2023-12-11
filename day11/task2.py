8047ab6706a95d42754a7d2992ac0ea599d34fa4
Tomasz Rzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2023-12-11 15:25:33 +0100

    syntax

--- day11/task2.py
+++ day11/task2.py
@@ -3,12 +3,14 @@ with open("input.txt", "r") as f:
 
 empty_space_dist = 1_000_000
 
+
 def empty_columns(galaxy_map: list[list[str]]) -> list[int]:
     empty_columns_list = []
     for i in range(len(galaxy_map[0])):
         if not "#" in [row[i] for row in galaxy_map]:
             empty_columns_list.append(i)
     return set(empty_columns_list)
+
 
 def empty_rows(galaxy_map: list[list[str]]) -> list[int]:
     empty_rows_list = []
@@ -17,24 +19,33 @@ def empty_rows(galaxy_map: list[list[str]]) -> list[int]:
             empty_rows_list.append(i)
     return set(empty_rows_list)
 
+
 def count_paths_to_other_nodes(
-    galaxy_map: list[list[str]], node_i: int, node_j: int,empty_rows_set:list, empty_columns_set:set
+    galaxy_map: list[list[str]],
+    node_i: int,
+    node_j: int,
+    empty_rows_set: list,
+    empty_columns_set: set,
 ) -> int:
     paths_length = 0
     for i in range(node_i, len(galaxy_map)):
         for j in range(len(galaxy_map[0])):
             if galaxy_map[i][j] == "#" and not (node_i == i and node_j == j):
-                min_i, max_i = min(node_i,i+1), max(node_i, i+1)
-                min_j, max_j = min(node_j,j+1), max(node_j,j+1)
-                count_empty_rows = len(set(range(min_i,max_i)) & empty_rows_set)
-                count_empty_columns = len(set(range(min_j,max_j)) & empty_columns_set)
-                paths_length += abs(i - node_i) + abs(j - node_j) + (empty_space_dist-1)*(count_empty_rows + count_empty_columns)
+                min_i, max_i = min(node_i, i + 1), max(node_i, i + 1)
+                min_j, max_j = min(node_j, j + 1), max(node_j, j + 1)
+                count_empty_rows = len(set(range(min_i, max_i)) & empty_rows_set)
+                count_empty_columns = len(set(range(min_j, max_j)) & empty_columns_set)
+                paths_length += (
+                    abs(i - node_i)
+                    + abs(j - node_j)
+                    + (empty_space_dist - 1) * (count_empty_rows + count_empty_columns)
+                )
     return paths_length, galaxy_map
 
 
 def count_paths_lengths(galaxy_map: list[list[str]]) -> int:
     empty_columns_set = empty_columns(galaxy_map)
-    empty_rows_set = empty_rows(galaxy_map) 
+    empty_rows_set = empty_rows(galaxy_map)
     path_lengths = 0
     for i in range(len(galaxy_map)):
         for j in range(len(galaxy_map[0])):
